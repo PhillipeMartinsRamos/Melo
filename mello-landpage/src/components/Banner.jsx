78df23cab3ab0aaf6af69f72a291eb81cbb5f88e
Lipe Martins Dev <lipe_martins@icloud.com>
2024-03-08 23:22:54 +0000

    video tag corrected

--- mello-landpage/src/components/Banner.jsx
+++ mello-landpage/src/components/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
         <h2>DOMINE O FLUXO</h2>
         <p>Soluções de Gestao de Tráfego para alavancar seu sucesso online</p>
       </div>
-      <Vi deo bannerTextElement={bannerTextElement} />
+      <Video bannerTextElement={bannerTextElement} />
     </section>
   );
 };
