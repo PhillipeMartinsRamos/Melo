f4c9d8eafb4138ee5c4c4e78360294dbb59b0f9b
Lipe Martins Dev <lipe_martins@icloud.com>
2024-03-09 00:07:26 +0000

    finished navbar

--- mello-landpage/src/components/Banner.jsx
+++ mello-landpage/src/components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
   const bannerTextElement = useRef()
   
   return (
-    <section className="banner">
+    <section className="banner" id="banner">
       <div className="banner-text" ref={bannerTextElement}>
         <h2>DOMINE O FLUXO</h2>
         <p>Soluções de Gestao de Tráfego para alavancar seu sucesso online</p>
